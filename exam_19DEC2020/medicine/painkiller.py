f9c080bb19132f73be0d991f9172d52c1003af53
Nowy <nowotny1993@gmail.com>
2022-01-23 17:58:46 +0200

    exam_19DEC2020_oop

---
+++ exam_19DEC2020/medicine/painkiller.py
@@ -0,0 +1,7 @@
+from python_oop_exams.project_19DEC2020.medicine.medicine import Medicine
+
+
+class Painkiller(Medicine):
+
+    def __init__(self):
+        super().__init__(20)
---
+++ exam_19DEC2020/supply/food_supply.py
@@ -0,0 +1,8 @@
+from python_oop_exams.project_19DEC2020.supply.supply import Supply
+
+
+class FoodSupply(Supply):
+
+    def __init__(self):
+        super().__init__(20)
+
