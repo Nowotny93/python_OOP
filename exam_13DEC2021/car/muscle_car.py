862bfce44bdbcb2cb7b1ef48c3f05c5cb56175a7
Nowy <nowotny1993@gmail.com>
2022-01-30 18:42:11 +0200

    exam_13DEC2021_oop

---
+++ exam_13DEC2021/car/muscle_car.py
@@ -0,0 +1,9 @@
+from python_oop_exams.project_13DEC2021.car.car import Car
+
+
+class MuscleCar(Car):
+    _MINIMUM_SPEED_LIMIT = 250
+    _MAXIMUM_SPEED_LIMIT = 451
+
+    def __init__(self, model, speed_limit):
+        super().__init__(model, speed_limit)
